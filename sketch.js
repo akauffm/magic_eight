248575525ae4245e89734556ab52ca0f6db3e3c9
Alex Kauffmann <alex@milkus.com>
2016-05-26 18:03:12 -0700

    block centering still bugging out

--- sketch.js
+++ sketch.js
@@ -1,47 +1,40 @@
-var sketch = function(p) {
-
   var x = 100; 
   var y = 100;
   var grammar, yaml, lines;
 
-  p.preload = function() {
-  	yaml = p.loadStrings('strings/haiku.yaml');
+  preload = function() {
+  	yaml = loadStrings('strings/haiku.yaml');
   };
 
-  p.setup = function() {
-    var canvas = p.createCanvas(650, 200);
+  setup = function() {
+    var canvas = createCanvas(650, 200);
     canvas.class("displayed");
     canvas.parent('p5sketch');
     select('canvas').style("visibility","visible");
-    p.textSize(30);
-    p.fill(255);
-    p.textAlign(p.CENTER);
-    p.noStroke();
+    textSize(30);
+    fill(255);
+    textAlign(CENTER);
+    noStroke();
     grammar = new RiGrammar(yaml.join('\n'));
     lines = [" ", " ", " "];
   };
 
-  p.draw = function() {
-  	p.background(0);
-  	p.text(lines[0], p.width/2, 75);
-  	p.text(lines[1], p.width/2, 110);
-  	p.text(lines[2], p.width/2, 145);
-
+  draw = function() {
+  	background(0);
+  	text(lines[0], width/2, 75);
+  	text(lines[1], width/2, 110);
+  	text(lines[2], width/2, 145);
   };
 
-
-  p.mousePressed = function() {
-    if (!p.fullscreen()) p.fullscreen(true);
+  mousePressed = function() {
+    if (!fullscreen()) fullscreen(true);
 };
 
-  p.deviceTurned = function() {
-  if (p.turnAxis == 'Z'){
+  deviceTurned = function() {
+  if (turnAxis == 'Z'){
     var result = grammar.expand();
   	var haiku = result.split("%");
   	for (var i = 0; i < lines.length; i++)
   		lines[i] = haiku[i];
   }
 };
-};
-
-var myp5 = new p5(sketch);
